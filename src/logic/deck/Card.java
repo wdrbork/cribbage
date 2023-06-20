460a511ef00729695d2a1eb10096d951dfe15699
Riley Bork <wdrb2001@gmail.com>
2023-06-20 16:17:35 -0700

    Implement Card class

--- src/logic/deck/Card.java
+++ src/logic/deck/Card.java
@@ -1,5 +1,34 @@
 package logic.deck;
 
 public class Card {
-    
+    private final Suit suit;
+    private final Rank rank;
+
+    public Card(Suit s, Rank r) {
+        suit = s;
+        rank = r;
+    }
+
+    public Suit getSuit() {
+        return this.suit;
+    }
+
+    public Rank getRank() {
+        return this.rank;
+    }
+
+    public int getValue() {
+        switch (rank) {
+            case ACE: return 1;
+            case TWO: return 2;
+            case THREE: return 3;
+            case FOUR: return 4;
+            case FIVE: return 5;
+            case SIX: return 6;
+            case SEVEN: return 7;
+            case EIGHT: return 8;
+            case NINE: return 9;
+            default: return 10;
+        }
+    }
 }
