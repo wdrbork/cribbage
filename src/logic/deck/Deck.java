d0ecaddb06a3edfa3586811e13dee4c11a85b5b5
Riley Bork <wdrb2001@gmail.com>
2023-06-20 16:45:50 -0700

    Implement constructor and fields for Deck object

--- src/logic/deck/Deck.java
+++ src/logic/deck/Deck.java
@@ -1,5 +1,25 @@
 package logic.deck;
 
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Represents a standard 52-card deck
+ */
 public class Deck {
-    
+    private static final int DECK_SIZE = 52;
+
+    private final List<Card> cards;
+    private int topCard;
+
+    public Deck() {
+        cards = new ArrayList<Card>(DECK_SIZE);
+        for (Suit suit : Suit.values()) {
+            for (Rank rank : Rank.values()) {
+                cards.add(new Card(suit, rank));
+            }
+        }
+
+        topCard = 0;
+    }
 }
