6e26d830df8c34633c0148f4a05d60ef1f65cb8b
wdrbork <wdrb2001@gmail.com>
2023-06-20 18:01:26 -0700

    Add comments to methods in Card.java

--- src/logic/deck/Card.java
+++ src/logic/deck/Card.java
@@ -1,12 +1,18 @@
 package logic.deck;
 
 /**
- * Represents a single card in a standard 52-card deck
+ * Represents a single card in a standard 52-card deck.
  */
 public class Card {
     private final Suit suit;
     private final Rank rank;
 
+    /**
+     * Creates a card using the given suit and rank.
+     * 
+     * @param s the suit of this card
+     * @param r the rank of this card
+     */
     public Card(Suit s, Rank r) {
         suit = s;
         rank = r;
@@ -20,6 +26,13 @@ public class Card {
         return this.rank;
     }
 
+    /**
+     * Returns the value of this card based on its rank. If the rank is a 
+     * number, that number is returned. Otherwise, aces return a value of 1, 
+     * and face cards (jacks, queens, and kings) return a value of 10.
+     * 
+     * @return the value of this card based on its rank
+     */
     public int getValue() {
         switch (rank) {
             case ACE: return 1;
--- src/logic/deck/Suit.java
+++ src/logic/deck/Suit.java
@@ -1,6 +1,6 @@
 package logic.deck;
 
-/* Includes all four suits in a standard 52-card deck */
+/* Includes all four suits in a standard 52-card deck. */
 public enum Suit {
     DIAMOND, HEART, CLUB, SPADE;
 }
