7d423d6c01d49011dfcd4f436c9c7daa49c7d5c8
wdrbork <wdrb2001@gmail.com>
2023-06-22 14:43:34 -0700

    Add to CribbageManager (for real this time)

--- src/logic/game/CribbageManager.java
+++ src/logic/game/CribbageManager.java
@@ -8,8 +8,12 @@ import logic.deck.Deck;
 /**
  * Manages a game of cribbage
  */
-public class CribbageGame {
+public class CribbageManager {
     private static final int MAX_COUNT = 31;
+    private static final int MAX_SCORE = 121;
+    private static final int TWO_PLAYER_START_SIZE = 6;
+    private static final int THREE_PLAYER_START_SIZE = 5;
+    private static final int HAND_SIZE = 4;
 
     private final int numPlayers;
     private final Deck deck;
@@ -18,15 +22,20 @@ public class CribbageGame {
     private final List<List<Card>> hands;
     private final List<Card> crib;
 
-    private Card starterCard;  // The starter card drawn at the end of the first stage
-    private int dealer;        // Represents a player's ID (between 0 and numPlayers exclusive)
-    private int count;         // Count for the second stage of play
+    // The starter card drawn at the end of the first stage
+    private Card starterCard;  
+
+    // Represents a player's ID (between 0 and numPlayers exclusive)
+    private int dealerId; 
+    
+    // Count for the second stage of play
+    private int count;         
 
     /**
      * Sets up a cribbage game with a random dealer.
      * @param numPlayers the number of players in the game (can only be 2 or 3)
      */
-    public CribbageGame(int numPlayers) {
+    public CribbageManager(int numPlayers) {
         this(numPlayers, (int) (Math.random() * numPlayers));
     }
 
@@ -35,10 +44,10 @@ public class CribbageGame {
      * @param numPlayers the number of players in the game (can only be 2 or 3)
      * @param dealer the ID of the dealer
      */
-    public CribbageGame(int numPlayers, int dealer) {
+    public CribbageManager(int numPlayers, int dealerId) {
         if (numPlayers != 2 || numPlayers != 3) {
             throw new IllegalArgumentException("Must have either 2 or 3 players");
-        } else if (dealer < 0 || dealer >= numPlayers) {
+        } else if (dealerId < 0 || dealerId >= numPlayers) {
             throw new IllegalArgumentException("Invalid ID for the dealer");
         }
 
@@ -46,13 +55,52 @@ public class CribbageGame {
         deck = new Deck();
         matchScores = new int[numPlayers - 1];
         gameScores = new int[numPlayers - 1];
-        hands = new ArrayList<List<Card>>();
-        crib = new ArrayList<Card>();
-        this.dealer = dealer;
+        hands = new ArrayList<List<Card>>(numPlayers);
+
+        /* The size of the starting hand is dependent on the number of players
+        in the game. 3 players = 5 cards, 2 players = 6 cards. */
+        int startSize = TWO_PLAYER_START_SIZE;
+        if (numPlayers == 3) {
+            startSize = THREE_PLAYER_START_SIZE;
+        }
+
+        /* Initialize the hands */
+        for (int i = 0; i < numPlayers; i++) {
+            hands.add(new ArrayList<Card>(startSize));
+        }
+
+        crib = new ArrayList<Card>(HAND_SIZE);
+        this.dealerId = dealerId;
     }
 
+    /**************************************************************************
+    * First Stage (Deal)
+    **************************************************************************/
     public void dealHands() {
+        checkEmptyHands();
 
+        int handSize = TWO_PLAYER_START_SIZE;
+        if (this.numPlayers == 3) {
+            handSize = THREE_PLAYER_START_SIZE;
+        }
+
+        /* Shuffle the deck and distribute cards to each player in the game. 
+           The player to the left of the dealer is dealt the first card */ 
+        deck.shuffle();
+        for (int i = 0; i < handSize; i++) {
+            for (int j = 1; j <= this.numPlayers; j++) {
+                Card next = deck.takeTopCard();
+                assert(next != null) : "Deck is empty";
+                hands.get((j + dealerId) % numPlayers).add(next);
+            }
+        }
+
+        /* If there are three players, the crib starts out with one card. */
+        if (this.numPlayers == 3) {
+            Card next = deck.takeTopCard();
+            assert(next != null) : "Deck is empty";
+            crib.add(next);
+        }
     }
 
     public void sendCardToCrib(int pid, Card card) {
@@ -67,12 +115,80 @@ public class CribbageGame {
         crib.add(card);
     }
 
+    public Card getStarterCard() {
+        checkFullHands();
+
+        Card starter = deck.takeTopCard();
+        assert(starter != null) : "Deck is empty";
+        starterCard = starter;
+        return starter;
+    }
+
+    /**************************************************************************
+    * Second Stage (Play)
+    **************************************************************************/
+
+    /**************************************************************************
+    * Third Stage (Show)
+    **************************************************************************/
     public int countHand(List<Card> hand) {
+        return count15Combos(hand) + countRuns(hand) + countPairs(hand)
+                + countFlush(hand) + countNobs(hand);
+    }
+
+    public void addPoints(int pid, int total) {
+        gameScores[pid] += total;
+
+        // 
+    }
+
+    public boolean isWinner(int pid) {
+
+    }
+
+    private int count15Combos(List<Card> hand) {
         return -1;
-        
+    }
+
+    private int countRuns(List<Card> hand) {
+        return -1;
     }
 
     private int countPairs(List<Card> hand) {
-        return 0;
+        return -1;
+    }
+
+    private int countFlush(List<Card> hand) {
+        return -1;
+    }
+
+    private int countNobs(List<Card> hand) {
+        return -1;
+    }
+
+    /* Ensures that all hands as well as the crib are empty */
+    private void checkEmptyHands() {
+        for (int i = 0; i < hands.size(); i++) {
+            if (!hands.get(i).isEmpty()) {
+                throw new IllegalStateException("Player(s) still have cards in their hand");
+            }
+        }
+
+        if (!crib.isEmpty()) {
+            throw new IllegalStateException("Crib still has cards");
+        }
+    }
+
+    /* Ensures that all hands as well as the crib are of size HAND_SIZE */
+    private void checkFullHands() {
+        for (int i = 0; i < hands.size(); i++) {
+            if (hands.get(i).size() != HAND_SIZE) {
+                throw new IllegalStateException("Hands are not finalized");
+            }
+        }
+
+        if (crib.size() != HAND_SIZE) {
+            throw new IllegalStateException("Crib does not have four cards");
+        }
     }
 }
