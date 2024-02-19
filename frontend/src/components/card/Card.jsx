7799663cf207c29729fbc5924af102f51d5b8da6
wdrbork <wdrb2001@gmail.com>
2024-02-19 14:39:49 -0800

    Add width and height to Card component

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -1,16 +1,21 @@
 import "./card.css";
 
+const CARD_WIDTH = 150;
+const CARD_HEIGHT_RATIO = 1.452;
+const PATH = "/images/PNG-cards-1.3/";
+
 function Card({ cardInfo }) {
   const cardImage =
-    "/images/PNG-cards-1.3/" +
-    cardInfo.rankValue +
-    "_of_" +
-    cardInfo.suit.toLowerCase() +
-    "s.png";
+    PATH + cardInfo.rankValue + "_of_" + cardInfo.suit.toLowerCase() + "s.png";
 
   return (
     <div className="Card">
-      <img src={cardImage} alt={cardImage} />
+      <img
+        src={cardImage}
+        width={CARD_WIDTH}
+        height={CARD_WIDTH * CARD_HEIGHT_RATIO}
+        alt={cardInfo.rank + " of " + cardInfo.suit + "S"}
+      />
     </div>
   );
 }
