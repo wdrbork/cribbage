3ec7037e24582b1d7b4e731d6d4b5afe34788c54
wdrbork <wdrb2001@gmail.com>
2024-02-19 15:27:31 -0800

    Request cribbage game scores from Game rather than Scoreboard

--- frontend/src/components/game/Game.jsx
+++ frontend/src/components/game/Game.jsx
@@ -1,8 +1,35 @@
 import "./game.css";
+import api from "../../api/axiosConfig.js";
 import Scoreboard from "../scoreboard";
 import Card from "../card";
+import { useState, useEffect } from "react";
+
+// Game Stages
+const DRAW_DEALER = 0;
+const DEAL_CRIB = 1;
+const PLAY_ROUND = 2;
+const COUNT_HANDS = 3;
+const COUNT_CRIB = 4;
 
 function Game({ numPlayers }) {
+  const [currentStage, setCurrentStage] = useState(0);
+  const [dealer, setDealer] = useState(-1);
+  const [gameScores, setGameScores] = useState(Array(numPlayers));
+
+  const getScores = async () => {
+    try {
+      const response = await api.get("/game/scores");
+      console.log(response);
+      setGameScores(response.data);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  useEffect(() => {
+    getScores();
+  }, [currentStage]);
+
   const cardInfo = {
     suit: "DIAMOND",
     rank: "SIX",
@@ -13,7 +40,7 @@ function Game({ numPlayers }) {
 
   return (
     <div className="Game">
-      <Scoreboard numPlayers={numPlayers} />
+      <Scoreboard gameScores={gameScores} />
       <Card cardInfo={cardInfo} />
     </div>
   );
--- frontend/src/components/scoreboard/Scoreboard.jsx
+++ frontend/src/components/scoreboard/Scoreboard.jsx
@@ -1,24 +1,8 @@
 import "./scoreboard.css";
-import api from "../../api/axiosConfig.js";
 import PlayerRow from "./playerRow";
 import { useState, useEffect } from "react";
 
-function Scoreboard({ numPlayers }) {
-  const [gameScores, setGameScores] = useState([0, 0]);
-
-  const getScores = async () => {
-    try {
-      const response = await api.get("/game/scores");
-      setGameScores(response.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  useEffect(() => {
-    getScores();
-  }, []);
-
+function Scoreboard({ gameScores }) {
   const scoreElements = gameScores.map((score, pid) => {
     return <PlayerRow key={pid} pid={pid} score={score} />;
   });
