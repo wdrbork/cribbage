831c747dc9770595cc86b339e3ba98ba8047a607
wdrbork <wdrb2001@gmail.com>
2024-04-27 12:46:13 -0700

    Use classes to style Card components rather than hard-coded values

--- frontend/src/components/card/Card.jsx
+++ frontend/src/components/card/Card.jsx
@@ -30,17 +30,13 @@ function Card({
       cardInfo.suit.toLowerCase() +
       "s.png";
 
-  let cardStyle = {
-    position: "absolute",
-  };
+  // const handleMouseOver = () => {
+  //   interactable && setOnHover(true);
+  // };
 
-  const handleMouseOver = () => {
-    interactable && setOnHover(true);
-  };
-
-  const handleMouseOut = () => {
-    interactable && setOnHover(false);
-  };
+  // const handleMouseOut = () => {
+  //   interactable && setOnHover(false);
+  // };
 
   const handleClick = () => {
     if (interactable) {
@@ -48,30 +44,32 @@ function Card({
     }
   };
 
-  if (interactable && (onHover || selected)) {
-    if (selected) {
-      cardStyle["top"] = "-20px";
-    } else if (onHover) {
-      cardStyle["top"] = "-5px";
-    }
+  let className = interactable ? "Card interactable" : "Card";
+
+  if (interactable && selected) {
+    className += " selected";
   }
 
+  // if (interactable) {
+  //   if (selected) {
+  //     cardStyle["top"] = "-20px";
+  //   } else if (onHover) {
+  //     cardStyle["top"] = "-5px";
+  //   }
+  // }
+
   if (!display) {
-    cardStyle["display"] = "none";
+    className += " no-display";
   }
 
   return (
-    <div
-      className="Card"
-      onMouseOver={handleMouseOver}
-      onMouseOut={handleMouseOut}
-      onClick={handleClick}
-    >
+    <div className="card-container">
       <img
+        className={className}
         src={cardImage}
         width={CARD_WIDTH}
         height={CARD_WIDTH * CARD_HEIGHT_RATIO}
-        style={cardStyle}
+        onClick={handleClick}
         alt={
           hidden ? "Back of card" : cardInfo.rank + " of " + cardInfo.suit + "S"
         }
