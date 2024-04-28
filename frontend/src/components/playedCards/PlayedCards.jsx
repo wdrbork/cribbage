0ffa6094ae5a48c88871a79784e057533873bff6
wdrbork <wdrb2001@gmail.com>
2024-04-28 12:36:32 -0700

    Create PlayedCards component

--- frontend/src/components/playedCards/PlayedCards.jsx
+++ frontend/src/components/playedCards/PlayedCards.jsx
@@ -1,8 +1,27 @@
-import './PlayedCards.css';
-import { useState } from 'react';
+import "./playedCards.css";
+import Card from "../card";
+// import { useState } from "react";
 
-function PlayedCards() {
-    
+function PlayedCards({ cards, oldCards }) {
+  function extractCardComponents() {
+    let playedCards = [];
+
+    oldCards.forEach((card) => {
+      playedCards.push(
+        <Card key={card.cardId} id={card.cardId} cardInfo={card} shaded />
+      );
+    });
+
+    cards.forEach((card) => {
+      playedCards.push(
+        <Card key={card.cardId} id={card.cardId} cardInfo={card} />
+      );
+    });
+
+    return playedCards;
+  }
+
+  return <div className="PlayedCards">{extractCardComponents()}</div>;
 }
 
 export default PlayedCards;
---
+++ frontend/src/components/playedCards/index.js
@@ -0,0 +1,1 @@
+export { default } from "./PlayedCards.jsx";
